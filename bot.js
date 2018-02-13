5cb022e238cfb15c7baa69f70f997d11877dd27c
Unknown <kgoforth07@gmail.com>
2018-02-13 10:32:20 -0600

    Updated Time
    No longer using intervals for tweets. (Every x seconds). 
    Now using a clock-based system. (At 12:00).
    This will run it at a normal time instead of it being somewhat random.

--- bot.js
+++ bot.js
@@ -1,19 +1,17 @@
 console.log("Bot Starting...");
-// Library setup and twit OAuth.
+// Setup:
+var schedule = require('node-schedule');
 const Twit = require('twit');
 const config = require('./config');
 const T = new Twit(config);
 console.log("Bot Authenticated!");
 
-dailyTweet();
-
-var intervalID = setInterval(dailyTweet, 86400000); //Interval for when the dailyTweet function is ran. (Every x seconds.)
-//Daily Random Number: 
-function dailyTweet() {
+var j = schedule.scheduleJob('* * 12 * * *', function(asdf){
     //Generate Random Number                   Max         Min
     let randNum = Math.floor((Math.random() * 1000000000) + 0);
     let percentProb = (randNum/1000000000)*100;
     let finalProb = percentProb.toFixed(3) + "%";
+
     T.post('statuses/update', { status: "Z0F\'s Daily random number is: " + randNum + "\nGenerating a number less than or equal to today's random number is a " + finalProb + " chance!" + "\n\nThe number is randomly generated between 0 and 1 Billion. \nMore info here: https://github.com/kgoforth/z0ftwitterbot1" + "\n#Random #Z0F #Z0FRandomNumber #Code"}, function(err, data, response) {    
         //If there is an error: Log it. Else: Say the tweet was successful and continue.
         if(err){
@@ -24,4 +22,4 @@ function dailyTweet() {
             console.log(data);
         }
     });
-}
+});
--- botTest.js
+++ botTest.js
@@ -1,27 +1,28 @@
-console.log("Bot Starting...");
+/*console.log("Bot Starting...");
 // Library setup and twit OAuth.
 const Twit = require('twit');
 const config = require('./config');
 const T = new Twit(config);
 console.log("Bot Authenticated!");
+*/
 
-/*dailyTweet();
+var schedule = require('node-schedule');
 
-var intervalID = setInterval(dailyTweet, 10000); //Interval for when the dailyTweet function is ran. (Every x seconds.)
-
-function dailyTweet() {
+var j = schedule.scheduleJob('20 * * * * *', function(asdf){
     //Generate Random Number                   Max         Min
     let randNum = Math.floor((Math.random() * 1000000000) + 0);
     let percentProb = (randNum/1000000000)*100;
     let finalProb = percentProb.toFixed(3) + "%";
-    //console.log(randNum);
-    //console.log("Generating a num lessthan or equal to this number is a", finalProb, "chance!");
 
     doStuff();
     function doStuff(){
         console.log("Z0F\'s Daily random number is: " + randNum + "\nGenerating a number less than or equal to today's random number is a " + finalProb + " chance!" + "\n\nThe number is randomly generated between 0 and 1 Billion. \nMore info here: https://github.com/kgoforth/z0ftwitterbot1" + "\n#Random #Z0F #Z0FRandomNumber #Code");
     }
-}*/
+});
+
+
+
+
 /*var stream = T.stream('user');
 stream.on('tweet', botCommands);
 botCommands();
