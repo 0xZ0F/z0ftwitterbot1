75fac1de972c839164a0f036093a8297f065ae77
Unknown <kgoforth07@gmail.com>
2018-02-13 10:39:16 -0600

    Clean Up
    Updated Version, added more/better comments, correctly added libraries.

--- bot.js
+++ bot.js
@@ -6,12 +6,14 @@ const config = require('./config');
 const T = new Twit(config);
 console.log("Bot Authenticated!");
 
+// Run the function at the set time. Second, Minute, Hour, Day, Week, Month.
 var j = schedule.scheduleJob('* * 12 * * *', function(asdf){
     //Generate Random Number                   Max         Min
     let randNum = Math.floor((Math.random() * 1000000000) + 0);
+    //Generate probablility of a number being generated lessthan or equal to then generated number.
     let percentProb = (randNum/1000000000)*100;
     let finalProb = percentProb.toFixed(3) + "%";
-
+    //Post the random number to Twitter.
     T.post('statuses/update', { status: "Z0F\'s Daily random number is: " + randNum + "\nGenerating a number less than or equal to today's random number is a " + finalProb + " chance!" + "\n\nThe number is randomly generated between 0 and 1 Billion. \nMore info here: https://github.com/kgoforth/z0ftwitterbot1" + "\n#Random #Z0F #Z0FRandomNumber #Code"}, function(err, data, response) {    
         //If there is an error: Log it. Else: Say the tweet was successful and continue.
         if(err){
--- botTest.js
+++ botTest.js
@@ -7,13 +7,13 @@ console.log("Bot Authenticated!");
 */
 
 var schedule = require('node-schedule');
-
+// Run the function at the set time. Second, Minute, Hour, Day, Week, Month.
 var j = schedule.scheduleJob('20 * * * * *', function(asdf){
     //Generate Random Number                   Max         Min
     let randNum = Math.floor((Math.random() * 1000000000) + 0);
     let percentProb = (randNum/1000000000)*100;
     let finalProb = percentProb.toFixed(3) + "%";
-
+    //This is in a function to simulate the post function on the live version.
     doStuff();
     function doStuff(){
         console.log("Z0F\'s Daily random number is: " + randNum + "\nGenerating a number less than or equal to today's random number is a " + finalProb + " chance!" + "\n\nThe number is randomly generated between 0 and 1 Billion. \nMore info here: https://github.com/kgoforth/z0ftwitterbot1" + "\n#Random #Z0F #Z0FRandomNumber #Code");
@@ -22,7 +22,7 @@ var j = schedule.scheduleJob('20 * * * * *', function(asdf){
 
 
 
-
+///TODO: Commands
 /*var stream = T.stream('user');
 stream.on('tweet', botCommands);
 botCommands();
