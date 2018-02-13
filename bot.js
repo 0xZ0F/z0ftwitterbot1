a8711e0aa9fb4f7bbffd10ce8900a1522b7ecac0
Unknown <kgoforth07@gmail.com>
2018-02-12 21:07:47 -0600

    Updated Bot
    Updated the bot with the new features :)

--- bot.js
+++ bot.js
@@ -12,8 +12,9 @@ var intervalID = setInterval(dailyTweet, 86400000); //Interval for when the dail
 function dailyTweet() {
     //Generate Random Number                   Max         Min
     let randNum = Math.floor((Math.random() * 1000000000) + 0);
-    
-    T.post('statuses/update', { status: 'Z0F\'s Daily random number is: ' + randNum + "\n\nThe random number is randomly generated between 0 and 1 Billion. \nMore info here: https://github.com/kgoforth/z0ftwitterbot1" + "\n#Random #Z0F #Z0FRandomNumber #Z0FsRandomNumber"}, function(err, data, response) {    
+    let percentProb = (randNum/1000000000)*100;
+    let finalProb = percentProb.toFixed(3) + "%";
+    T.post('statuses/update', { status: "Z0F\'s Daily random number is:" + randNum + "\nGenerating a number less than or equal to today's random number is a" + finalProb + "chance!" + "\n\nThe number is randomly generated between 0 and 1 Billion. \nMore info here: https://github.com/kgoforth/z0ftwitterbot1" + "\n#Random #Z0F #Z0FRandomNumber #Code"}, function(err, data, response) {    
         //If there is an error: Log it. Else: Say the tweet was successful and continue.
         if(err){
             console.error("There was an error posting a tweet! Here is the error:");
